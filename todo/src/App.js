ad82a7cc12c18af15811469c614acd24906c401d
Dino-Muratovic <dinomuratovic1989@gmail.com>
2020-05-11 13:46:36 -0500

    Installed dependencies and started server. first push to my branch

---
+++ todo/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     This is app
+    </div>
+  );
+}
+
+export default App;
